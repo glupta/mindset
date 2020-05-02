16f4fb0f9cca1713fb807bd8b8cb97c3de24e2de
Daniel Rozycki <drozycki16@gmail.com>
2020-05-02 11:19:22 -0700

    remove references to HelloWorld

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="landing-page">
+  <div class="home">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
@@ -85,7 +85,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Home',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
