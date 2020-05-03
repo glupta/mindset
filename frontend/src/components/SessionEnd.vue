3ad1e154d7bfed2af0ab09a674c82ef133dce712
Sam <samrsabri@gmail.com>
2020-05-03 15:47:21 +0300

    implemented sessionEnd

--- frontend/src/components/SessionEnd.vue
+++ frontend/src/components/SessionEnd.vue
@@ -1,31 +1,28 @@
 <template>
     <div class='session-end-screen'>
       <NavBar></NavBar>
-        <p class='header'>
-            Your Session Has Ended!
-        </p>
-        <p class='description'>
+        <br><br><br>
+        <p class="header">Your session has ended!</p>
+        <br><br>
+        <p class='smalltext'>
             Did your buddy participate in the meditation practice?
         </p>
+        <br>
         <div class='rating-section'>
+          <br><br>
+          <div style="position:relative; height:100%;">
+            <div style="float: left; width: 15%;height: 40px;width: 40px;"></div>
             <div class='yes-button' v-bind:class="{ active: yesSelected}"  v-on:click='onSelectYes()'>
-                <div class='thumbs-up'></div>
-                <p class='yes-text'>
-                    Yes
-                </p>
+              <p style="margin-top:1.5em; color:#696969;">Yes</p>
             </div>
+            <div style="float: left; width: 5%;height: 40px;width: 40px;"></div>
             <div class='no-button' v-bind:class="{ active: noSelected }"  v-on:click='onSelectNo()'>
-                <div class='thumbs-down'></div>
-                <p class='no-text'>
-                    No
-                </p>
+              <p style="margin-top:1.5em; color:#696969;">No</p>
             </div>
+          </div>
         </div>
-        <div class='schedule-appointment-button' v-on:click='goToScheduleSession()'>
-            <p class="schedule-appointment-text">
-                Make Another Appointment
-            </p>
-        </div>
+        <br><br>
+        <a class='schedule-appointment-button'  v-on:click='goToScheduleSession()'>New Session</a>
     </div>
 </template>
 
@@ -61,19 +58,53 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.session-end-screen {
+  font-family: 'DIN Condensed', sans-serif;
+  font-size: 24px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.smalltext {
+  font-size: 14px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.rating-section {
+  width: 317px;
+  height: 194px;
+  left: 26px;
+  top: 322px;
+  background: rgba(196, 196, 196, 0.15);
+  border-radius: 6px;
+  margin: 0 auto;
+  text-align: center;
 }
-a {
-  color: #42b983;
+
+.yes-button, .no-button{
+  float: left;
+  border: 2px solid #18A0FB;
+  height: 100px;
+  width: 100px;
+  border-radius: 6px;
+  background-color: #c7e9ff;
+  color: #18A0FB;
+}
+
+.yes-button {
+}
+
+.no-button {
+  
+}
+
+.schedule-appointment-button {
+  font-size: 18px;
+  font-family: 'DIN Condensed', sans-serif;
+  border-radius: 5px;
+  border: 2px solid #18A0FB;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-right: 95px;
+  padding-left: 95px;
+  color: #18a0fb;
 }
 
 .active {
